64cfa53f4f2bf837ed93ac46fd9a7f182cfa658c
ccocz <resulhangeldiyev@gmail.com>
2021-02-15 00:01:58 +0100

    Finish first multi-threaded version

--- src/multiThreadedPageRankComputer.hpp
+++ src/multiThreadedPageRankComputer.hpp
@@ -1,7 +1,6 @@
 #ifndef SRC_MULTITHREADEDPAGERANKCOMPUTER_HPP_
 #define SRC_MULTITHREADEDPAGERANKCOMPUTER_HPP_
 
-#include <atomic>
 #include <mutex>
 #include <thread>
 
@@ -18,10 +17,61 @@ public:
     MultiThreadedPageRankComputer(uint32_t numThreadsArg)
         : numThreads(numThreadsArg) {};
 
-    std::vector<PageIdAndRank> computeForNetwork(Network const& /*network*/, double /*alpha*/, uint32_t /*iterations*/, double /*tolerance*/) const
+    std::vector<PageIdAndRank> computeForNetwork(Network const& network, double alpha, uint32_t iterations, double tolerance) const
     {
-        std::vector<PageIdAndRank> result;
-    	return result;
+        std::unordered_map<PageId, PageRank, PageIdHash> pageHashMap;
+        for (auto const& page : network.getPages()) {
+            page.generateId(network.getGenerator());
+            pageHashMap[page.getId()] = 1.0 / network.getSize();
+        }
+
+        std::unordered_map<PageId, uint32_t, PageIdHash> numLinks;
+        for (auto page : network.getPages()) {
+            numLinks[page.getId()] = page.getLinks().size();
+        }
+
+        std::unordered_set<PageId, PageIdHash> danglingNodes;
+        for (auto page : network.getPages()) {
+            if (page.getLinks().size() == 0) {
+                danglingNodes.insert(page.getId());
+            }
+        }
+
+        std::unordered_map<PageId, std::vector<PageId>, PageIdHash> edges;
+        for (auto page : network.getPages()) {
+            for (auto link : page.getLinks()) {
+                edges[link].push_back(page.getId());
+            }
+        }
+
+        std::vector<PageId> division[numThreads];
+        uint32_t last = 0;
+
+        for (auto& pageMapElem : pageHashMap) {
+            last += division[last].size() == network.getSize() / numThreads + 1;
+            division[last].push_back(pageMapElem.first);
+        }
+
+        IterationHelper helper(alpha, network, edges, numLinks, numThreads, division, danglingNodes);
+
+        double difference;
+
+        for (uint32_t i = 0; i < iterations; ++i) {
+            std::unordered_map<PageId, PageRank, PageIdHash> previousPageHashMap = pageHashMap;
+            difference = helper.newIteration(previousPageHashMap, pageHashMap);
+
+            std::vector<PageIdAndRank> result;
+            for (auto iter : pageHashMap) {
+                result.push_back(PageIdAndRank(iter.first, iter.second));
+            }
+
+            ASSERT(result.size() == network.getSize(), "Invalid result size=" << result.size() << ", for network" << network);
+
+            if (difference < tolerance) {
+                return result;
+            }
+        }
+        ASSERT(false, "Not able to find result in iterations=" << iterations);
     }
 
     std::string getName() const
@@ -31,6 +81,82 @@ public:
 
 private:
     uint32_t numThreads;
+
+    class IterationHelper {
+
+    public:
+        IterationHelper(double alpha,
+                        Network const& network,
+                        std::unordered_map<PageId, std::vector<PageId>, PageIdHash>& edges,
+                        std::unordered_map<PageId, uint32_t, PageIdHash>& numLinks,
+                        uint32_t numThreads,
+                        std::vector<PageId>* division,
+                        std::unordered_set<PageId, PageIdHash> danglingNodes)
+            : numThreads(numThreads)
+            , alpha(alpha)
+            , network(network)
+            , edges(edges)
+            , numLinks(numLinks)
+            , division(division)
+            , danglingNodes(danglingNodes) {};
+
+        double newIteration(std::unordered_map<PageId, PageRank, PageIdHash>& previousPageHashMap,
+            std::unordered_map<PageId, PageRank, PageIdHash>& pageHashMap)
+        {
+            dangleSum = 0;
+            for (auto danglingNode : danglingNodes) {
+                dangleSum += previousPageHashMap[danglingNode];
+            }
+            dangleSum = dangleSum * alpha;
+
+            double difference = 0;
+
+            std::vector<std::thread> threads;
+
+            for (uint32_t i = 0; i < numThreads; i++) {
+                threads.push_back(std::thread(&IterationHelper::workerThread,
+                    this,
+                    std::ref(division[i]),
+                    std::ref(previousPageHashMap),
+                    std::ref(pageHashMap),
+                    std::ref(difference)));
+            }
+            for (auto& thread : threads) {
+                thread.join();
+            }
+            return difference;
+        }
+
+    private:
+        uint32_t numThreads;
+        double dangleSum;
+        double alpha;
+        Network const& network;
+        std::unordered_map<PageId, std::vector<PageId>, PageIdHash>& edges;
+        std::unordered_map<PageId, uint32_t, PageIdHash>& numLinks;
+        std::vector<PageId>* division;
+        std::mutex mutex;
+        std::unordered_set<PageId, PageIdHash> danglingNodes;
+
+        void workerThread(const std::vector<PageId>& myDivision,
+            std::unordered_map<PageId, PageRank, PageIdHash>& previousPageHashMap,
+            std::unordered_map<PageId, PageRank, PageIdHash>& pageHashMap,
+            double& difference)
+        {
+            for (PageId pageId : myDivision) {
+                double danglingWeight = 1.0 / network.getSize();
+                double myPR = dangleSum * danglingWeight + (1.0 - alpha) / network.getSize();
+                if (edges.count(pageId) > 0) {
+                    for (auto link : edges[pageId]) {
+                        myPR += alpha * previousPageHashMap[link] / numLinks[link];
+                    }
+                    std::lock_guard<std::mutex> lockGuard(mutex);
+                    pageHashMap[pageId] = myPR;
+                    difference += std::abs(previousPageHashMap[pageId] - pageHashMap[pageId]);
+                }
+            }
+        }
+    };
 };
 
 #endif /* SRC_MULTITHREADEDPAGERANKCOMPUTER_HPP_ */
